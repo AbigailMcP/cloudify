7c96ba0b97c2584506e6ae6e6db5944100f569cf
Abigail <abigail.mcphillips@gmail.com>
2016-11-12 14:06:25 +0000

    adds resources for wordcloud

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'wordcloud/new'
-
-  get 'wordcloud/create'
-
-  get 'wordcloud/show'
-
+  resources :wordcloud, only: [:new, :create, :show]
   root 'wordcloud#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
