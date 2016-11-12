bdca1083fe48b2257a6807cd7c0e92e91c1207e0
Abigail <abigail.mcphillips@gmail.com>
2016-11-12 17:32:25 +0000

    model methods to generate hash of words and word counts

--- app/models/wordcloud.rb
+++ app/models/wordcloud.rb
@@ -1,8 +1,18 @@
 class Wordcloud < ApplicationRecord
   validates :username, presence: true
 
+  def word_count
+    count = Hash.new(0)
+    all_words.each {|word| count[word] += 1}
+    count
+  end
+
+  def all_words
+    tweets.map {|tweet| tweet.text.split(' ')}.flatten
+  end
+
   def tweets
-    options = {:count => 10, :include_rts => true}
+    options = {:count => 20, :include_rts => true}
     TWITTER.user_timeline(username, options)
   end
 
