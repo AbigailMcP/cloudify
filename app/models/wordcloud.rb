dba2a8c5e395aea9f56f3f9d9b791444571b5955
Abigail <abigail.mcphillips@gmail.com>
2016-11-13 11:33:54 +0000

    adds methods to filter our stopwords from file

--- app/models/wordcloud.rb
+++ app/models/wordcloud.rb
@@ -1,16 +1,28 @@
 class Wordcloud < ApplicationRecord
   validates :username, presence: true
 
+  # STOPWORDS = File.readlines('./app/assets/stopwords.txt')
+  # STOPWORDS2 = STOPWORDS.map(&:strip)
+
   def word_count
     count = Hash.new(0)
-    all_words.each {|word| count[word] += 1}
+    filtered_words.each {|word| count[word] += 1}
     count
   end
 
   private
 
+  def filtered_words
+    all_words - stop_words
+  end
+
+  def stop_words
+    stopwords = File.readlines('./app/assets/stopwords.txt')
+    stopwords_strip = stopwords.map(&:strip)
+  end
+
   def all_words
-    tweets.map {|tweet| tweet.text.split(' ')}.flatten
+    tweets.map {|tweet| tweet.text.downcase.split(' ')}.flatten
   end
 
   def tweets
